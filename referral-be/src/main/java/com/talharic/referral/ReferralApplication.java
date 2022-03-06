08bf9d295d661c76862d52e94e5a71f2a9a9f089
Talha Arıç <talharic@gmail.com>
2022-03-06 03:12:29 +0300

    Initial commit

---
+++ referral-be/src/main/java/com/talharic/referral/ReferralApplication.java
@@ -0,0 +1,13 @@
+package com.talharic.referral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferralApplication.class, args);
+    }
+
+}
